99caa30561562eaf3e7987b8e88dc3af8792dd34
Veronicapark09 <devegaisabelle29@gmail.com>
2024-05-20 11:51:36 +0800

    Update DICE ROLLER.js

--- DICE ROLLER.js
+++ DICE ROLLER.js
@@ -8,7 +8,7 @@ function rollDice(){
     const values = [];
     const images = [];
 
-    for(let i = 0; i< numOfDice; i++){
+    for(let i = 0; i < numOfDice; i++){
         const value = Math.floor(Math.random() * 6) + 1;
         values.push(value);
         images.push(`<img src="dice images/${value}.png" alt="Dice ${value}.png">`);
